a66516c709668f5fe07106220e05e0de28b7d060
swatisbci <swatisbci@gmail.com>
2023-01-02 16:57:26 -0500

    Added Allure reporting to Runner.java

--- src/test/java/com/runner/Runner.java
+++ src/test/java/com/runner/Runner.java
@@ -3,11 +3,12 @@ package com.runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/resources/features",glue ="com.stepDefinations"
-,monochrome=true, tags =("@ds or @linkedlist or @arrays"),
-//tags = "@ds", dryRun = false, 
+@CucumberOptions(features="src/test/resources/features"
+,glue ={"com.stepDefinations","com.utils.Helper"}
+,monochrome=true,dryRun = false,   // tags="@arrays",
 
 plugin= {"pretty","html:target/cucumber.html"
+		,"html:target/HtmlReports.html","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
 })
 public class Runner extends AbstractTestNGCucumberTests {
 	
